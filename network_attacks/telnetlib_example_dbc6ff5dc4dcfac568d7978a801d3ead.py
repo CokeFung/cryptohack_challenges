3a0a81bb16dfbafb1ab6005de4a9241cf9c902e0
6ued <naruebetcm@gmail.com>
2020-11-03 21:39:32 +0700

    solved network attacks

---
+++ network_attacks/telnetlib_example_dbc6ff5dc4dcfac568d7978a801d3ead.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python3
+
+import telnetlib
+import json
+
+HOST = "socket.cryptohack.org"
+PORT = 11112
+
+tn = telnetlib.Telnet(HOST, PORT)
+
+
+def readline():
+    return tn.read_until(b"\n")
+
+def json_recv():
+    line = readline()
+    return json.loads(line.decode())
+
+def json_send(hsh):
+    request = json.dumps(hsh).encode()
+    tn.write(request)
+
+# readline()
+# readline()
+# readline()
+# readline()
+print(readline())
+print(readline())
+print(readline())
+print(readline())
+
+
+request = {
+    "buy": "flag"
+}
+json_send(request)
+
+response = json_recv()
+
+print(response)
+
+
